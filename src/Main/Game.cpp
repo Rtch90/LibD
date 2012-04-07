e8c4dec5d2b8716779ae9e57ab75caa15990a0f1
Rtch90 <ritchie.cunningham@protonmail.com>
2012-04-07 22:33:25 +0100

    [Fix] Fixed Makefiles. [Announcement] Tamir has joined the LibD development. :D

--- src/Main/Game.cpp
+++ src/Main/Game.cpp
@@ -38,7 +38,7 @@ void Game::Render(void) {
 	glRotatef(_rotationAngle, 0, 0, 1);
 	
 	glBegin(GL_TRIANGLES);
-		glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+	  glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
 		glVertex3f(-1.0f, -0.5f, -4.0f);
 		glColor4f(1.0f, 1.0f, 0.0f, 1.0f);
 		glVertex3f(1.0f, -0.5f, -4.0f);
